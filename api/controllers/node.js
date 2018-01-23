a4f217612f660643479b0086a60b153758d46d62
Mona Baerenfaenger <mona@lightcurve.io>
2018-01-23 16:00:04 +0100

    Remove generic description, adjust @todos

--- api/controllers/node.js
+++ api/controllers/node.js
@@ -31,7 +31,7 @@ var config;
  * @requires module:helpers/swagger.generateParamsErrorObject
  * @requires module:helpers/swagger.invalidParams
  * @param {Object} scope - App instance.
- * @TODO: add description of the module
+ * @todo: add description of NodeController
  */
 function NodeController (scope) {
 	modules = scope.modules;
@@ -42,7 +42,7 @@ function NodeController (scope) {
  * @public
  * @param {Object} context
  * @param {function} next
- * @TODO: add description of the function and its parameters
+ * @todo: add description of the function and its parameters
  */
 NodeController.getConstants = function (context, next) {
 	modules.node.shared.getConstants(null, function (err, data) {
@@ -78,7 +78,7 @@ NodeController.getConstants = function (context, next) {
  * @public
  * @param {Object} context
  * @param {function} next
- * @TODO: add description of the function and its parameters
+ * @todo: add description of the function and its parameters
  */
 NodeController.getStatus = function (context, next) {
 	modules.node.shared.getStatus(null, function (err, data) {
@@ -109,7 +109,7 @@ NodeController.getStatus = function (context, next) {
  * @public
  * @param {Object} context
  * @param {function} next
- * @TODO: add description of the function and its parameters
+ * @todo: add description of the function and its parameters
  */
 NodeController.getForgingStatus = function (context, next) {
 	if (!checkIpInList(config.forging.access.whiteList, context.request.ip)) {
@@ -130,7 +130,7 @@ NodeController.getForgingStatus = function (context, next) {
  * @public
  * @param {Object} context
  * @param {function} next
- * @TODO: add description of the function and its parameters
+ * @todo: add description of the function and its parameters
  */
 NodeController.updateForgingStatus = function (context, next) {
 	if (!checkIpInList(config.forging.access.whiteList, context.request.ip)) {
@@ -155,7 +155,7 @@ NodeController.updateForgingStatus = function (context, next) {
  * @public
  * @param {Object} context
  * @param {function} next
- * @TODO: add description of the function and its parameters
+ * @todo: add description of the function and its parameters
  */
 NodeController.getPooledTransactions = function (context, next) {
 	var invalidParams = swaggerHelper.invalidParams(context.request);
